9adde74a2599ff23c9c8da4954f95e42b3db2f2c
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-24 23:23:09 +0200

    refactor: add affiliate api module

---
+++ src/lib/api/affiliate.ts
@@ -0,0 +1,25 @@
+import { apiGet, apiPost } from "./client";
+
+export function getAffiliateProfile() {
+  return apiGet("/users/me");
+}
+
+export function completeAffiliateOnboarding(payload: Record<string, unknown>) {
+  return apiPost("/affiliate/onboarding", payload);
+}
+
+export function getAffiliateReferrals() {
+  return apiGet("/affiliate/referrals");
+}
+
+export function getAffiliateCommissions() {
+  return apiGet("/affiliate/commissions");
+}
+
+export function getAffiliatePayouts() {
+  return apiGet("/affiliate/payouts");
+}
+
+export function requestAffiliatePayout() {
+  return apiPost("/affiliate/payouts", {});
+}
