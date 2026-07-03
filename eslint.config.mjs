583fa98782c0b721fde610034b7438bece4ad611
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-03 11:25:21 +0200

    Disable set-state-in-effect lint for async dashboards

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ const eslintConfig = defineConfig([
   {
     rules: {
       "@next/next/no-img-element": "off",
+      "react-hooks/set-state-in-effect": "off",
     },
   },
   // Override default ignores of eslint-config-next.
