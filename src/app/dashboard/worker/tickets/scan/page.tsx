582c6cc56ecfa3c4cfe8e9c832b7903320eb537c
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-04 03:47:30 +0200

    Add worker dashboard ticket scan route

---
+++ src/app/dashboard/worker/tickets/scan/page.tsx
@@ -0,0 +1,5 @@
+import { DashboardTicketsWorkspace } from "@/components/tickets/DashboardTicketsWorkspace";
+
+export default function WorkerTicketScanRoute() {
+  return <DashboardTicketsWorkspace role="Worker" />;
+}
