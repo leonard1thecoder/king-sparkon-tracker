7feb43542fab308eb0e0bf83aba44483e377dc37
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-09 10:53:28 +0200

    Add user tuck shop cart route

---
+++ src/app/dashboard/user/shop/cart/page.tsx
@@ -0,0 +1,9 @@
+import { TuckShopCartDashboard } from "@/components/tuck-shop/TuckShopCartDashboard";
+
+export default function UserTuckShopCartPage() {
+  return (
+    <main className="grid gap-6 p-5 md:p-8">
+      <TuckShopCartDashboard />
+    </main>
+  );
+}
