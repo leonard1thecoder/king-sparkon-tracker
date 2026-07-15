5e8414c16ce17b879a3cc3c212c60c15083667f4
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-07-15 10:40:42 +0200

    Add pinned GSAP browser runtime

---
+++ public/king-sparkon-gsap-runtime.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import { gsap } from "https://esm.sh/gsap@3.13.0";
+
+window.__KING_SPARKON_MOTION__ = { gsap };
+window.dispatchEvent(new CustomEvent("king-sparkon:motion-ready"));
